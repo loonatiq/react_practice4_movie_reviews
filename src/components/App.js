0c30a96d531f58261db27d888f132bceb882c67f
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-01-12 23:25:08 +0900

    addsort

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
 
 function App() {
+  const [sort, setSort] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[sort] - a[sort]);
+
+  const handleNewestClick = () => setSort("createdAt");
+  const handleBestClick = () => setSort("rating");
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleBestClick}>평점순</button>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
