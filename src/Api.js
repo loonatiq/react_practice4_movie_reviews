5ec934b45f3f72c920b76cbfb9376b5c2a644d88
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-01-17 22:28:08 +0900

    add addmore & paging

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,15 @@
-export async function getReviews(sort = "createdAt") {
-  const query = `sort=${sort}`;
+export async function getReviews({
+  sort = "createdAt",
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `sort=${sort}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
     `https://learn.codeit.kr/api/film-reviews?${query}`
   );
+  if (!response.ok) {
+    throw new Error("리뷰를 불러오는데 실패했습니다.");
+  }
   const body = await response.json();
   return body;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import ReviewList from "./ReviewList";
 import "./App.css";
 import { getReviews } from "../Api";
 
+const LIMIT = 6;
+
 function App() {
   const [items, setItems] = useState([]);
   const [sort, setSort] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
   const sortedItems = items.sort((a, b) => b[sort] - a[sort]);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
   const handleNewestClick = () => setSort("createdAt");
   const handleBestClick = () => setSort("rating");
@@ -16,14 +22,35 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async (sortQuery) => {
-    const { reviews } = await getReviews(sortQuery);
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
   };
 
-  //무한루프 방지
+  const handleLoadMore = () => {
+    handleLoad({ sort, offset, limit: LIMIT });
+  };
+
+  //무한루프 방지, 초기실행
   useEffect(() => {
-    handleLoad(sort);
+    handleLoad({ sort, offset: 0, limit: LIMIT });
   }, [sort]);
 
   return (
@@ -32,10 +59,17 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>평점순</button>
       </div>
-      <div></div>
       <div className="main">
         <ReviewList items={sortedItems} onDelete={handleDelete} />
       </div>
+      <div className="addMore">
+        {hasNext && (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            더 보기
+          </button>
+        )}
+      </div>
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
